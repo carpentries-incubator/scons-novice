bb3146d5f7ff17ca1a6f6974eaa9a74720b27055
Kyle Brindley <kbrindley@lanl.gov>
2024-12-20 09:13:54 -0700

    WIP: working Glob from python module

--- episodes/files/code/06-variables/scons_lesson_configuration.py
+++ episodes/files/code/06-variables/scons_lesson_configuration.py
@@ -1,11 +1,11 @@
 import pathlib
 
-import SCons
+import SCons.Script
 
 COUNT_SOURCE = "countwords.py"
 LANGUAGE = "python"
 ZIPF_SOURCE = "testzipf.py"
-TEXT_FILES = Glob("books/*.txt")
+TEXT_FILES = SCons.Script.Glob("books/*.txt")
 
 
 def count_words(env, data_file, language=LANGUAGE, count_source=COUNT_SOURCE):
