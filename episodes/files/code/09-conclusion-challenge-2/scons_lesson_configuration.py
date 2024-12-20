dccb7a480c44a4651cc81395f82e237d0594b449
Kyle Brindley <kbrindley@lanl.gov>
2024-12-20 10:16:26 -0700

    WIP: match conclusion challenge to two file project configuration

--- episodes/files/code/09-conclusion-challenge-2/scons_lesson_configuration.py
+++ episodes/files/code/09-conclusion-challenge-2/scons_lesson_configuration.py
@@ -6,24 +6,32 @@ COUNT_SOURCE = "countwords.py"
 LANGUAGE = "python"
 ZIPF_SOURCE = "testzipf.py"
 TEXT_FILES = SCons.Script.Glob("books/*.txt")
+DATA_FILES = [
+    pathlib.Path(str(text_file)).with_suffix(".dat").name
+    for text_file in TEXT_FILES
+]
 
 
-def count_words(env, data_file, language=LANGUAGE, count_source=COUNT_SOURCE):
+def count_words(env, data_files, language=LANGUAGE, count_source=COUNT_SOURCE):
     """Pseudo-builder to produce `.dat` targets from the `countwords.py` script
 
-    Assumes that the source text file is found in `books/{data_file}.txt`
+    Assumes that the source text files are found in `books/{data_file}.txt`
 
     :param env: SCons construction environment. Do not provide when using this
         function with the `env.AddMethod` and `env.CountWords` access style.
-    :param data_file: String name of the data file to create.
+    :param data_files: List of string names of the data files to create.
     """
-    data_path = pathlib.Path(data_file)
-    text_file = pathlib.Path("books") / data_path.with_suffix(".txt")
-    target_nodes = env.Command(
-        target=[data_file],
-        source=[text_file, count_source],
-        action=["${language} ${count_source} ${SOURCES[0]} ${TARGET}"],
-        language=language,
-        count_source=count_source,
-    )
+    target_nodes = []
+    for data_file in data_files:
+        data_path = pathlib.Path(data_file)
+        text_file = pathlib.Path("books") / data_path.with_suffix(".txt")
+        target_nodes.extend(
+            env.Command(
+                target=[data_file],
+                source=[text_file, count_source],
+                action=["${language} ${count_source} ${SOURCES[0]} ${TARGET}"],
+                language=language,
+                count_source=count_source,
+            )
+        )
     return target_nodes
